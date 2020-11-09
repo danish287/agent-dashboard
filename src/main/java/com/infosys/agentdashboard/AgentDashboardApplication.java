3f71d7e574f702d9b8897212617dba2f167ccb65
danish287 <ale.estrada99@gmail.com>
2020-11-09 13:57:46 -0700

    first commit

---
+++ src/main/java/com/infosys/agentdashboard/AgentDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.agentdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosys/agentdashboard/AgentDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.agentdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
