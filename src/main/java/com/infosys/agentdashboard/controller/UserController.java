a8bf633bf412e0ed8b44f095b9b64885b9734456
danish287 <ale.estrada99@gmail.com>
2020-11-10 11:14:08 -0700

    begin working on angular

--- src/main/java/com/infosys/agentdashboard/controller/UserController.java
+++ src/main/java/com/infosys/agentdashboard/controller/UserController.java
@@ -2,14 +2,12 @@ package com.infosys.agentdashboard.controller;
 
 import com.infosys.agentdashboard.entity.UserEntity;
 import com.infosys.agentdashboard.models.User;
-import com.infosys.agentdashboard.service.UserService;
+import com.infosys.agentdashboard.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
 @RequestMapping("/users")
 @RestController
